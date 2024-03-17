74659f1320ffba040e180b1822ec7faa3b9a876a
WhyAre <laiyr52@gmail.com>
2024-03-17 13:19:52 +0800

    Enable format on save on conform

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -162,6 +162,10 @@ local plugins = {
                 formatters_by_ft = {
                     python = { "isort", "black" },
                 },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
             })
         end,
     },
