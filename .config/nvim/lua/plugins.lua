3dfdc4841d7b31542608218260f4320376875216
WhyAre <laiyr52@gmail.com>
2024-03-17 21:07:22 +0800

    Increase conform format on save timeout

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -163,7 +163,8 @@ local plugins = {
                     python = { "isort", "black" },
                 },
                 format_on_save = {
-                    timeout_ms = 500,
+                    -- These options will be passed to conform.format()
+                    timeout_ms = 1000,
                     lsp_fallback = true,
                 },
             })
