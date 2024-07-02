570fc5c79c1d44e41dac217eee591e22a8acb506
WhyAre <laiyr52@gmail.com>
2024-07-02 09:35:34 +0800

    Add formatting options for various languages

--- .config/nvim/plugin/ftplugin.lua
+++ .config/nvim/plugin/ftplugin.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spelllang = "en"
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"typst", "tex", "markdown"},
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en"
+  end,
+})
