418c1bea3ef989380f53e7b28cbea0bc59d3e5fd
WhyAre <laiyr52@gmail.com>
2024-07-16 22:26:36 +0800

    Disable K mapping on LspAttach
    https://www.reddit.com/r/neovim/comments/1d6g3va/lsp_errors_in_neovim_0100/

--- .config/nvim/lua/whyare/lsp.lua
+++ .config/nvim/lua/whyare/lsp.lua
@@ -17,27 +17,30 @@ vim.diagnostic.config({
 
 vim.keymap.set("n", "<leader>li", '<cmd>LspInfo<CR>', { desc = "LSP Info" })
 
-local on_attach = function(_)
-    vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = 'Code actions' })
-    vim.keymap.set("n", "<leader>ld", require "telescope.builtin".diagnostics, { desc = 'Diagnostics' })
-    vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float, { desc = 'Diagnostics' })
-    vim.keymap.set("n", "<leader>lf", function()
-        require("conform").format()
-    end, { desc = 'Format' })
-    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = 'Rename' })
-    vim.keymap.set("n", '<c-]>', require "telescope.builtin".lsp_definitions,
-        { desc = 'Go to definition' })
-    vim.keymap.set("n", ']d', vim.diagnostic.goto_next, { desc = 'Next error' })
-    vim.keymap.set("n", '[d', vim.diagnostic.goto_prev, { desc = 'Previous error' })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = 'Open definition' })
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = 'Go to declaration' })
-    vim.keymap.set("n", "gI", require "telescope.builtin".lsp_implementations,
-        { desc = 'Go to implementation' })
-    vim.keymap.set("n", "gT", require "telescope.builtin".lsp_type_definitions,
-        { desc = 'Go to type definition' })
-    vim.keymap.set("n", "gd", require "telescope.builtin".lsp_definitions, { desc = 'Go to definition' })
-    vim.keymap.set("n", "gr", require "telescope.builtin".lsp_references, { desc = 'Go to references' })
-end
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('whyare-lsp-attach', { clear = true }),
+    callback = function(event)
+        vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = 'Code actions' })
+        vim.keymap.set("n", "<leader>ld", require "telescope.builtin".diagnostics, { desc = 'Diagnostics' })
+        vim.keymap.set("n", "<leader>lo", vim.diagnostic.open_float, { desc = 'Diagnostics' })
+        vim.keymap.set("n", "<leader>lf", function()
+            require("conform").format()
+        end, { desc = 'Format' })
+        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = 'Rename' })
+        vim.keymap.set("n", '<c-]>', require "telescope.builtin".lsp_definitions,
+            { desc = 'Go to definition' })
+        vim.keymap.set("n", ']d', vim.diagnostic.goto_next, { desc = 'Next error' })
+        vim.keymap.set("n", '[d', vim.diagnostic.goto_prev, { desc = 'Previous error' })
+        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = 'Open definition' })
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = 'Go to declaration' })
+        vim.keymap.set("n", "gI", require "telescope.builtin".lsp_implementations,
+            { desc = 'Go to implementation' })
+        vim.keymap.set("n", "gT", require "telescope.builtin".lsp_type_definitions,
+            { desc = 'Go to type definition' })
+        vim.keymap.set("n", "gd", require "telescope.builtin".lsp_definitions, { desc = 'Go to definition' })
+        vim.keymap.set("n", "gr", require "telescope.builtin".lsp_references, { desc = 'Go to references' })
+    end
+})
 
 local configs = {
     gopls = { enabled = true },
@@ -82,7 +85,6 @@ local configs = {
 }
 
 local default_config = {
-    on_attach = on_attach,
 }
 
 local ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
