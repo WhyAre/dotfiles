2c9f9ab31f35ba68525019f64f9227d7c6ae27dc
WhyAre <laiyr52@gmail.com>
2025-05-28 23:53:43 +0800

    Enable fancy tab bar on wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,6 +12,7 @@ end
 local config = {
     font_size = 11.0,
     hide_tab_bar_if_only_one_tab = true,
+    use_fancy_tab_bar = false,
     window_padding = {
         left = 0,
         right = 0,
