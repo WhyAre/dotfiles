c5ed3b23f1514794a8254c564932d469aebaeee3
WhyAre <laiyr52@gmail.com>
2025-05-30 10:19:32 +0800

    Remove null_ls sources
    black and isort are handled by conform.nvim

--- .config/nvim/lua/whyare/null_ls.lua
+++ .config/nvim/lua/whyare/null_ls.lua
@@ -2,9 +2,6 @@
 local null_ls = require('null-ls')
 
 -- register any number of sources simultaneously
-local sources = {
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.isort
-}
+local sources = {}
 
 null_ls.setup({ sources = sources })
