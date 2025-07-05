2002e1ff08ff063ebeced37bfd3c05f5eaf0d89e
WhyAre <laiyr52@gmail.com>
2025-07-06 00:53:31 +0800

    Fix wrong lsp configuration

--- .config/nvim/lua/whyare/lsp.lua
+++ .config/nvim/lua/whyare/lsp.lua
@@ -43,7 +43,8 @@ local configured_lsps = {
     rust_analyzer = {
         settings = {
             ["rust-analyzer"] = {
-                checkOnSave = {
+                checkOnSave = true,
+                check = {
                     command = "clippy",
                 },
             },
