a59310e3e9db005bdfec2f8f0012dbdbfb0f0cec
WhyAre <laiyr52@gmail.com>
2025-12-23 02:08:28 +0800

    Fix scala metals config
    Idk why it was broken

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -293,6 +293,25 @@ local plugins = {
     'mfussenegger/nvim-jdtls',
 
     {
+        "scalameta/nvim-metals",
+        ft = { "scala", "sbt", "java" },
+        opts = function()
+            local metals_config = require("metals").bare_config()
+            return metals_config
+        end,
+        config = function(self, metals_config)
+            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = self.ft,
+                callback = function()
+                    require("metals").initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+            })
+        end
+    },
+
+    {
         'mfussenegger/nvim-dap',
         config = function()
             require("dapui").setup()
@@ -491,30 +510,6 @@ local plugins = {
             vim.g.copilot_no_tab_map = true
         end
     },
-
-    {
-        "scalameta/nvim-metals",
-        ft = { "scala", "sbt", "java" },
-        opts = function()
-            local metals_config = require("metals").bare_config()
-            metals_config.on_attach = function(client, bufnr)
-            -- your on_attach function
-            end
-            metals_config.init_options.statusBarProvider = "on"
-
-            return metals_config
-        end,
-        config = function(self, metals_config)
-            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-            vim.api.nvim_create_autocmd("FileType", {
-            pattern = self.ft,
-            callback = function()
-                require("metals").initialize_or_attach(metals_config)
-            end,
-            group = nvim_metals_group,
-            })
-        end
-    }
 }
 
 require("lazy").setup(plugins)
